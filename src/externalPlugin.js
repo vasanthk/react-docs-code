684278e00e93801db107242dfee341fae34ec913
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-03-31 12:52:10 +0530

    Code from External Plugin - React

---
+++ src/externalPlugin.js
@@ -0,0 +1,29 @@
+var MarkdownEditor = React.createClass({
+  getInitialState: function () {
+    return {value: 'Type some *markdown* here!'};
+  },
+  handleChange: function () {
+    this.setState({value: this.refs.textarea.value});
+  },
+  rawMarkup: function () {
+    return {__html: marked(this.state.value, {sanitize: true})};
+  },
+  render: function () {
+    return (
+      <div className="MarkdownEditor">
+        <h3>Input</h3>
+        <textarea
+          onChange={this.handleChange}
+          ref="textarea"
+          defaultValue={this.state.value}/>
+        <h3>Output</h3>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={this.rawMarkup()}
+        />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<MarkdownEditor />, document.getElementById('example'));
