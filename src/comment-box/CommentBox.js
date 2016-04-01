473f888655c6934311d8f48f229261689fc8c2c3
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-04-01 13:19:14 +0530

    Include comment list and comment form in comment box

--- src/comment-box/CommentBox.js
+++ src/comment-box/CommentBox.js
@@ -2,7 +2,9 @@ var CommentBox = React.createClass({
   render: function () {
     return (
       <div className="commentBox">
-        Hello, world! I am a comment box.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
