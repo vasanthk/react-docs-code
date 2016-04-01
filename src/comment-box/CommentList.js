1605f8a5ebbb683e1c00c73ed05233e2afaa1300
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-04-01 13:18:31 +0530

    Comment list component stub

---
+++ src/comment-box/CommentList.js
@@ -0,0 +1,9 @@
+var CommentList = React.createClass({
+  render: function () {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
