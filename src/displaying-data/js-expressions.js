5a914e691597033d3c46202893e3d18727888055
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-04-08 00:07:31 -0700

    JSX comments

--- src/displaying-data/js-expressions.js
+++ src/displaying-data/js-expressions.js
@@ -28,3 +28,17 @@ var content = React.createElement(
   null,
   window.isLoggedIn ? React.createElement(Nav) : React.createElement(Login)
 );
+
+// JSX comments
+
+var content = (
+  <Nav>
+    {/* child comment, put {} around */}
+    <Person
+      /* multi
+         line
+         comment */
+      name={window.isLoggedIn ? window.name : ''} // end of line comment
+    />
+  </Nav>
+);
