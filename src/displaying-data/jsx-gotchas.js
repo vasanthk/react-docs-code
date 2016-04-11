39919f0699adc760d98d9e9a5e7b7df5b070f158
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-04-10 23:52:52 -0700

    Unicode characters directly in JavaScript

--- src/displaying-data/jsx-gotchas.js
+++ src/displaying-data/jsx-gotchas.js
@@ -4,5 +4,8 @@
 // Bad: It displays "First &middot; Second"
 <div>{'First &middot; Second'}</div>
 
+// Unicode characters directly in JavaScript
+<div>{'First · Second'}</div>
+
 // Web Accessibility attributes starting with aria- will be rendered properly.
 <div aria-hidden={true} />
