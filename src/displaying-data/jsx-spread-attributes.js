eb98da877b8dead58b4fb1095fccd745c2fc544d
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-04-11 17:06:53 -0700

    Gotcha - In props later attributes override previous ones

--- src/displaying-data/jsx-spread-attributes.js
+++ src/displaying-data/jsx-spread-attributes.js
@@ -12,3 +12,11 @@ var props = {};
 props.foo = x;
 props.bar = y;
 var component = <Component {...props} />;
+
+// Gotcha:
+// You can use this multiple times or combine it with other attributes.
+// The specification order is important. Later attributes override previous ones.
+
+var props = { foo: 'default' };
+var component = <Component {...props} foo={'override'} />;
+console.log(component.props.foo); // 'override'
